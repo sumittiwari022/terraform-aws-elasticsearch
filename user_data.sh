2e438c06c8818ad48792a18ec3769de7d3b22a42
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-26 22:43:22 +0530

    added arm64 based

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 version=7.17.8
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-"$version"-x86_64.rpm
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-"$version"-x86_64.rpm.sha512
-shasum -a 512 -c elasticsearch-"$version"-x86_64.rpm.sha512
-sudo rpm --install elasticsearch-"$version"-x86_64.rpm
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-"$version"-aarch64.rpm
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-"$version"-aarch64.rpm.sha512
+shasum -a 512 -c elasticsearch-"$version"-aarch64.rpm.sha512
+sudo rpm --install elasticsearch-"$version"-aarch64.rpm
 
 sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch.service
